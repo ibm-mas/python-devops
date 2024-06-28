0f0da5889f848f3eb4190dad16670d852a4d5fb4
David Parker <parkerda@uk.ibm.com>
2024-06-28 09:53:15 +0100

    [patch] Updates to support mas install with ECK (#4)

--- setup.py
+++ setup.py
@@ -44,10 +44,7 @@ setup(
     author='David Parker',
     author_email='parkerda@uk.ibm.com',
     package_dir={'': 'src'},
-    packages=find_namespace_packages(where='src', include=[
-        'mas.devops',
-        'mas.devops.templates'
-    ]),
+    packages=find_namespace_packages(where='src'),
     include_package_data=True,
     url='https://github.com/ibm-mas/python-devops',
     license='Eclipse Public License - v1.0',
