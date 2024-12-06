fce3c7830081cd6442f1d3260c6e57e09309657e
Parveen Kumar <parveenkumar@ibm.com>
2024-12-06 19:32:14 +0530

    [minor] Support December catalog (#41)
    Co-authored-by: Anil Prajapati <Anil.Prajapati@ibm.com>
    Co-authored-by: David Parker <parkerda@uk.ibm.com>
    Co-authored-by: Ian Boden <ianboden@uk.ibm.com>
    Co-authored-by: Rawa <61942902+rawa-resul@users.noreply.github.com>

--- test/src/test_data.py
+++ test/src/test_data.py
@@ -12,6 +12,7 @@ from mas.devops.data import getCatalog, getNewestCatalogTag, listCatalogTags
 
 
 def test_catalog():
+    # We don't need to update this to the latest version each monthly update
     catalogData = getCatalog("v9-241107-amd64")
     assert catalogData["catalog_digest"] == "sha256:2d470131ab6948d5262553547fafa1b472fa25690be5abba8719ad7493cd8911"
 
@@ -19,13 +20,13 @@ def test_catalog():
 def test_list_catalogs():
     catalogList = listCatalogTags("amd64")
     assert len(catalogList) > 0
-    assert "v9-241107-amd64" in catalogList
+    assert "v9-241205-amd64" in catalogList
 
 
 def test_get_newest_catalog_tag():
     catalogTag = getNewestCatalogTag("amd64")
     # Reminder: update this test when adding a new catalog each month!
-    assert catalogTag == "v9-241107-amd64"
+    assert catalogTag == "v9-241205-amd64"
 
 
 def test_get_newest_catalog_tag_fail():
