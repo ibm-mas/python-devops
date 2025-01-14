49f0d03bc118e03602369a5f509112a0690f37f3
Rawa Resul <rawa.resul@ibm.com>
2025-01-14 14:29:17 +0000

    [patch] autopep8 fix

--- src/mas/devops/sls.py
+++ src/mas/devops/sls.py
@@ -28,5 +28,5 @@ def verifySLSConnection(sls_url: str, server_ca: str) -> bool:
     logger.info("Checking SLS connection")
     response = requests.get(f"{sls_url}api/probes/readiness", verify=server_ca)
     if response.status_code == 200:
-            return True
+        return True
     return False
