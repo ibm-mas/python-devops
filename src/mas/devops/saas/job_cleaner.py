83e8b53f73cc3ad10a092aed9092aa3173746728
klapitom@uk.ibm.com <7372253+tomklapiscak@users.noreply.github.com>
2025-02-13 18:10:20 +0000

    job  cleaner updates

--- src/mas/devops/saas/job_cleaner.py
+++ src/mas/devops/saas/job_cleaner.py
@@ -1,5 +1,5 @@
 # *****************************************************************************
-# Copyright (c) 2024 IBM Corporation and other Contributors.
+# Copyright (c) 2025 IBM Corporation and other Contributors.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
@@ -23,35 +23,20 @@ def cleanup_jobs(k8s_client: client.api_client.ApiClient, label: str):
     for cm in cms.items:
         cm_ns = cm.metadata.namespace
         job_cleanup_group = cm.metadata.labels[label]
+        logger.info("")
         logger.info(f"{job_cleanup_group} in {cm_ns}")
+        logger.info("-------------------------------")
         try:
             current_job_name = cm.data['current_job_name']
+            logger.info(f"Current Job Name: {current_job_name}")
 
             # get all Jobs in the same namespace as the configmap that have LABEL: job_cleanup_group
             jobs_in_cleanup_group = batch_v1_api.list_namespaced_job(cm_ns, label_selector=f"{label}={job_cleanup_group}")
 
-            # sanity checks, abort cleanup of this group if any of these fail
-            #   - one of the jobs should be named current_job_name
-            #   - all jobs names should have job_cleanup_group as a prefix
-            found_current_job = False
-            for job in jobs_in_cleanup_group.items:
-                job_name = job.metadata.name
-
-                if job_name == current_job_name:
-                    found_current_job = True
-
-                if not job_name.startswith(job_cleanup_group):
-                    raise Exception(f"Job name {job_name} has unexpected prefix")
-
-            if found_current_job:
-                logger.info(f"   Found current Job resource: {current_job_name}")
-            else:
-                raise Exception(f"Could not find current job {current_job_name}")
-
             for job in jobs_in_cleanup_group.items:
                 job_name = job.metadata.name
                 if job_name != current_job_name:
-                    logger.info(f"   Deleting old Job resource: {job_name}")
+                    logger.info(f"Deleting old Job resource: {job_name}")
 
         except Exception as e:
             logger.error(f"Skipping {job_cleanup_group} in {cm_ns}: {repr(e)}")
