cea3f791ae4d5986a9e5e96d889842289d59ec20
Shyam Doshi <Shyam.Doshi@ibm.com>
2026-06-11 23:43:18 +0530

    [patch] updated test for custom catalog

--- test/src/test_data.py
+++ test/src/test_data.py
@@ -32,7 +32,7 @@ def test_list_catalogs():
 def test_get_newest_catalog_tag():
     catalogTag = getNewestCatalogTag("amd64")
     # Reminder: update this test when adding a new catalog each month!
-    assert catalogTag == "v9-260625-amd64"
+    assert catalogTag == "v9-facilities-dev-amd64"
 
 
 def test_get_newest_catalog_tag_fail():
